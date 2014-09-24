e29ad767c8ec59073739122b78c59f5e33021b42
Dustin Buckenmeyer <dustin.buckenmeyer@gmail.com>
2014-09-24 12:28:15 -0400

    Fixed a bug with the Fancy Radios, and implemented a way to override/customize the entire plugin.

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
 TODO
 ====
 
-* Implement configuration function that can be used to override/customize anything
+* Test out using FancyCheckbox.prototype = new FancyBase(); so that the plugin will work on older browsers
+* Rework fancy events for select (potentially eliminate the need for closeAll)
+* Re-evaluate whether the blur timers are needed
 * Write in-depth technical documentation
 * Use GitHub pages to host demo and the technical doc
 * Test mobile
--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,27 @@
 $(function() {
+	// $.fancyOverrides({
+	// 	FancyCheckbox: {
+	// 		init: function () {
+	// 			console.log("FancyCheckbox init function overriden");
+	// 		}
+	// 	},
+	// 	FancyRadio: {
+	// 		init: function () {
+	// 			this.newFunction();
+	// 		},
+	// 		newFunction: function () {
+	// 			console.log("FancyRadio newFunction function introduced");
+	// 		}
+	// 	},
+	// 	FancySelect: {
+	// 		open: function () {
+	// 			this.fel.addClass("dd-open");
+	// 		},
+	// 		close: function () {
+	// 			this.fel.removeClass("dd-open");
+	// 		}
+	// 	}
+	// });
 	$("input:checkbox").fancifyCheckbox();
 	$("input:radio").fancifyRadio();
 	$("select").fancifySelect();
